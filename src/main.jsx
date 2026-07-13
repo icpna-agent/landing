2b6c24d3388b367d5143e5377fd84e40e5311234
Erick Flores Santos <xerickcua@gmail.com>
2026-07-12 23:09:37 -0500

    .

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css' // <-- Debe estar aquí 
 import App from './App.jsx'
 
-globalThis.__ICPNA_BACKEND_URL__ = import.meta.env.VITE_BACKEND_URL
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
